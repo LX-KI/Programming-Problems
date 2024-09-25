2086503693eee09358d0cbfc4e19283301866724
LX-KI <rlaukeek@gmail.com>
2024-09-25 19:18:43 +0530

    Linear Search

---
+++ src/com/Arrays/L_search.java
@@ -0,0 +1,19 @@
+package com.Arrays;
+
+public class L_search {
+	public static int linearSearch(int[] arr,int key) {
+		for(int i = 0 ; i<arr.length;i++) {
+			if(arr[i]==key){
+				return i;
+			}
+			
+			}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int [] arr = {10,20,30,40,50,55,60,70,80,90};
+		int key = 55;
+		int index = linearSearch(arr, key);
+		System.out.println(index);
+	}
+}
