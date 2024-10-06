a765ce406ed56f426e2b96851a05be44d266710e
LX-KI <rlaukeek@gmail.com>
2024-10-06 22:57:06 +0530

    Rotating an array in anti clkwise dir

--- src/com/Arrays/Anti_clk.java
+++ src/com/Arrays/Anti_clk.java
@@ -13,8 +13,8 @@ public class Anti_clk {
 	public static void rot_anti(int arr[]) {
 		int temp = arr[0];
 		
-		for(int i=1;i<arr.length;i++) {
-			arr[i-1]=arr[i];
+		for(int i=0;i<arr.length-1;i++) {
+			arr[i]=arr[i+1];
 		}
 		arr[arr.length-1]=temp;
 	}
